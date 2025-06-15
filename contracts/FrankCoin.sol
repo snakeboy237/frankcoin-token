ae248786a84a001fde56d650c2f31ba22e3a550b
snakeboy237 <mamssolide@gmail.com>
2025-06-14 21:58:36 -0400

    upcheck

--- contracts/FrankCoin.sol
+++ contracts/FrankCoin.sol
@@ -1,33 +1,37 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract FrankCoin is ERC20Burnable, Pausable, Ownable {
-    constructor(uint256 initialSupply)
-        ERC20("FrankCoin", "FRC")
-        Ownable(msg.sender)
-    {
-        require(initialSupply > 0, "Initial supply is required!");
-        _mint(msg.sender, initialSupply * (10 ** decimals()));
-    }
 
-    function pause() public onlyOwner {
-        _pause();
-    }
+// Creating Token
+contract FrankCoin is ERC20, ERC20Burnable, Pausable, AccessControl {
 
-    function unpause() public onlyOwner {
-        _unpause();
-    }
+    //Access control roles 
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant MINER_ROLE = keccak256("MINER_ROLE");
 
-    // Hook that runs before any transfer (including mint and burn)
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override
-    {
-        super._beforeTokenTransfer(from, to, amount);
-        require(!paused(), "FrankCoin: Token transfers are paused");
+    //Wallet to collect taxes
+    address public treasury ;
+    //Tax percentage 
+    uint256 public taxPercentage = 200;
+    //Tax whitelist map
+    mapping(address =>bool) public whitelist;
+
+    constructor(address _admin, address _treasury) ERC20("FrankCoin", "FRNC"){
+        
+        require(_admin != address(0) && _treasury != address(0),"Invalid arguments");  // avoiding empty addresses error during initiation
+        
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);  // Grants default role to admin
+        _grantRole(ADMIN_ROLE, _admin);  //Granted admin role to admin
+        _grantRole(MINER_ROLE, _admin);  // Granting miner role to admin
+
+        treasury = _treasury; //Setting the treasury address
+
     }
 }
